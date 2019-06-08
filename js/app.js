179baff4611656de54f05364bcd1101a312cf4ed
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-08 23:19:20 +0200

    make board, columns, and cards

--- js/app.js
+++ js/app.js
@@ -1,12 +1,46 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3959',
+  'X-Auth-Token': '6a532014e4790a933feb33e82e234ed3'
+};
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
+
+fetch(prefix + baseUrl + '/board', { headers: myHeaders, cache: 'no-store' })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(resp) {
+        setupColumns(resp.columns);
+    });
+
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
 }
+
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    })
+}
+
+// Ostatnim elementem jest dodanie karty do kolumny za pomocą metody createCard(), którą tworzyliśmy w module 11 (można ją znaleźć w pliku Column.js).
+
+
+// OGÓLNA FUNKCJA
+// function randomString() {
+// 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+// 	var str = '', i;
+// 	for (i = 0; i < 10; i++) {
+// 	  str += chars[Math.floor(Math.random() * chars.length)];
+// 	}
+// 	return str;
+// }
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -18,20 +52,20 @@ function generateTemplate(name, data, basicElement) {
   	return element;
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+// // TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
+// var todoColumn = new Column('Do zrobienia');
+// var doingColumn = new Column('W trakcie');
+// var doneColumn = new Column('Skończone');
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+// // DODAWANIE KOLUMN DO TABLICY
+// board.createColumn(todoColumn);
+// board.createColumn(doingColumn);
+// board.createColumn(doneColumn);
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+// // TWORZENIE NOWYCH EGZEMPLARZY KART
+// var card1 = new Card('Nowe zadanie');
+// var card2 = new Card('stworzyc tablice kanban');
 
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+// // DODAWANIE KART DO KOLUMN
+// todoColumn.createCard(card1);
+// doingColumn.createCard(card2);
--- js/column.js
+++ js/column.js
@@ -1,8 +1,8 @@
-function Column(name) {
+function Column(id, name) {
     var self = this;
 
-    this.id = randomString();
-    this.name = name;
+    this.id = id;
+    this.name = name || 'No name given';
     this.element = generateTemplate('column-template', { name: this.name, id: this.id });
 
     this.element.querySelector('.column').addEventListener('click', function (event) {
@@ -11,16 +11,42 @@ function Column(name) {
       }
   
       if (event.target.classList.contains('add-card')) {
-            self.addCard(new Card(prompt("Enter the name of the card")));
+        var cardName = prompt("Enter the name of the card");
+        event.preventDefault();
+        var data = new FormData();
+        data.append('name', cardName);
+        data.append('bootcamp_kanban_column_id', self.id);
+
+        fetch(prefix + baseUrl + '/card', { 
+            method: 'POST',
+            body: data,
+            cache: 'no-store'
+        })
+        .then(function(resp){
+            resp.json();
+        })
+        .then(function(resp){
+            var card = new Card(resp.id, cardName);
+            self.addCard(card);
+        });
       }
   });
 }
 
 Column.prototype = {
+
   addCard: function(card) {
     this.element.querySelector('ul').appendChild(card.element);
   },
+
   removeColumn: function() {
-    this.element.parentNode.removeChild(this.element);
+    var self = this;
+    fetch(prefix + baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders, cache: 'no-store' })
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(resp) {
+        self.element.parentNode.removeChild(self.element);
+      });
   }
 };
